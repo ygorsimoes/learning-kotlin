6f4d7a743edb41434543e37aa0d598b3694c213b
Ygor Simões <contato.ygorsimoes@gmail.com>
2022-11-02 22:57:09 -0300

    Added comments

--- src/main/kotlin/bytebank/Account.kt
+++ src/main/kotlin/bytebank/Account.kt
@@ -6,6 +6,7 @@ class Account(
     private var balance: Double
 ) {
 
+    // Return the extract of the account.
     fun extract(): String {
         return """
             =================================
@@ -16,6 +17,7 @@ class Account(
         """.trimIndent()
     }
 
+    // Deposit is a method that receives a value and adds it to the balance.
     fun deposit(value: Double): Boolean {
         if (value > 0) {
             this.balance += value
@@ -24,6 +26,7 @@ class Account(
         return false
     }
 
+    // Withdraw is a method that receives a value and subtracts it from the balance.
     fun withdraw(value: Double): Boolean {
         if (value > 0 && value <= this.balance) {
             this.balance -= value
@@ -32,6 +35,8 @@ class Account(
         return false
     }
 
+    // Transfer is a method that receives a value and subtracts it from the balance of the account that is making
+    // the transfer.
     fun transfer(destinationAccount: Account, value: Double): Boolean {
         if (destinationAccount.owner != this.owner) {
             if (value > 0 && value <= this.balance) {
