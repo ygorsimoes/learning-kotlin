12094681a8670099c8e89b8caa63dc1ca7e3559c
Ygor Simões <contato.ygorsimoes@gmail.com>
2023-06-29 03:54:12 -0300

    feat: add challenge "Fizz Buzz"

---
+++ src/main/kotlin/challenge/FizzBuzz.kt
@@ -0,0 +1,20 @@
+package challenge
+
+fun main() {
+    println(fizzBuzz(20))
+}
+
+fun fizzBuzz(n: Int): List<String> {
+    if (n <= 0 || n >= 100) return listOf("Please, numbers from 1 to 100!")
+
+    val listBuzz: List<String> = (1..n).map {
+        when {
+            it % 3 == 0 && it % 5 == 0 -> "FizzBuzz"
+            it % 3 == 0 -> "Fizz"
+            it % 5 == 0 -> "Buzz"
+            else -> "$it"
+        }
+    }
+
+    return listBuzz
+}
